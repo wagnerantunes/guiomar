e82369c46f7d6e8fa59a66ffc0e10ddd278c4bfb
wagnerantunes <wagnerantunes84@gmail.com>
2026-01-06 19:19:47 -0300

    perf: optimize script loading with preconnect and afterInteractive strategy

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,6 +81,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
 import { Providers } from "@/components/Providers";
 import { FloatingOrbs } from "@/components/ui/FloatingOrbs";
+import Script from "next/script";
 
 export default async function RootLayout({
   children,
@@ -139,88 +140,18 @@ export default async function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        {/* Preconnect to third-party domains */}
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="preconnect" href="https://connect.facebook.net" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
         />
 
-        {/* GOOGLE TAG MANAGER (HEAD) */}
-        {integrations.gtmId && integrations.gtmId.startsWith('GTM-') && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${integrations.gtmId}');
-              `
-            }}
-          />
-        )}
-
-        {/* GOOGLE ANALYTICS */}
-        {integrations.gaId && (
-          <>
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${integrations.gaId}`}></script>
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${integrations.gaId}');
-                `
-              }}
-            />
-          </>
-        )}
-
-        {/* FACEBOOK PIXEL */}
-        {integrations.fbPixelId && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${integrations.fbPixelId}');
-                fbq('track', 'PageView');
-              `
-            }}
-          />
-        )}
-
-        {/* TIKTOK PIXEL */}
-        {integrations.tiktokPixelId && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function (w, d, t) {
-                  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
-                  ttq.load('${integrations.tiktokPixelId}');
-                  ttq.page();
-                }(window, document, 'ttq');
-              `
-            }}
-          />
-        )}
-
-        {/* CUSTOM HEAD SCRIPTS */}
-        {integrations.customHead && (
-          <script
-            id="custom-head-scripts"
-            dangerouslySetInnerHTML={{
-              __html: integrations.customHead.replace(/<script\b[^>]*>([\s\S]*?)<\/script>/gim, '$1')
-            }}
-          />
-        )}
-
+        {/* Schema.org structured data */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -280,6 +211,91 @@ export default async function RootLayout({
         {/* CUSTOM BODY END SCRIPTS */}
         {integrations.customBodyEnd && (
           <div dangerouslySetInnerHTML={{ __html: integrations.customBodyEnd }} />
+        )}
+
+        {/* OPTIMIZED THIRD-PARTY SCRIPTS - Load after interactive */}
+        {integrations.gtmId && integrations.gtmId.startsWith('GTM-') && (
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${integrations.gtmId}');
+              `
+            }}
+          />
+        )}
+
+        {integrations.gaId && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${integrations.gaId}`}
+              strategy="afterInteractive"
+            />
+            <Script
+              id="ga-script"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${integrations.gaId}');
+                `
+              }}
+            />
+          </>
+        )}
+
+        {integrations.fbPixelId && (
+          <Script
+            id="fb-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${integrations.fbPixelId}');
+                fbq('track', 'PageView');
+              `
+            }}
+          />
+        )}
+
+        {integrations.tiktokPixelId && (
+          <Script
+            id="tiktok-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function (w, d, t) {
+                  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
+                  ttq.load('${integrations.tiktokPixelId}');
+                  ttq.page();
+                }(window, document, 'ttq');
+              `
+            }}
+          />
+        )}
+
+        {integrations.customHead && (
+          <Script
+            id="custom-head-scripts"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: integrations.customHead.replace(/<script\b[^>]*>([\s\S]*?)<\/script>/gim, '$1')
+            }}
+          />
         )}
       </body>
     </html>
